31a7a06403dcc91ba2536a3383f0fa13e980d4d0
Verona0609 <dikaya.nika15@gmail.com>
2024-04-01 15:29:32 +0200

    hw3

--- js/task-3.js
+++ js/task-3.js
@@ -1,4 +1,4 @@
-/* function filterArray(numbers, value){
+function filterArray(numbers, value){
    let newArray=[];
    for (let number of numbers){
       if(number>value){
@@ -13,5 +13,5 @@ console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
 console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
 console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
 console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
- */
+
 
